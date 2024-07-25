c62431cb4ba4a772aded4345d147c2d928f9b5e6
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2024-07-25 10:55:59 -0300

    Create algo8.c

---
+++ algo8.c
@@ -0,0 +1,19 @@
+/*8. Faça um algoritmo para ler três notas de um aluno em uma disciplina e imprimir a sua média
+ponderada (as notas tem pesos respectivos de 1, 2 e 3)*/
+#include<stdio.h>
+int main(void){
+    float nota1, nota2, nota3, media;
+    
+    printf("Insira a nota 1: ");
+    scanf("%f", &nota1);
+    
+    printf("Insira a bota 2: ");
+    scanf("%f", &nota2);
+    
+    printf("Insira a nota 3: ");
+    scanf("%f", &nota3);
+    
+    media = ((nota1*1)+(nota2*2)+(nota3*3))/6;
+    
+    printf("Média = %.2f", media);
+}
