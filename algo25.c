6a373fc02577f42d915aad12a434e646880c0447
Gabrielle Carvalho <82004840+gabrielle-carvalho@users.noreply.github.com>
2024-07-26 07:39:34 -0300

    Create algo25.c

---
+++ algo25.c
@@ -0,0 +1,14 @@
+/*25. Calcule o volume de uma caixa d'água cilíndrica.*/
+#include<stdio.h>
+int main(void){
+    float raio, altura, volume;
+    printf("Digite o raio: ");
+    scanf("%f", &raio);
+    
+    printf("Digite a altura: ");
+    scanf("%f", &altura);
+
+    volume=(3.14*(raio*raio)*altura);
+
+    printf("Resultado = %.2f", volume);
+}
